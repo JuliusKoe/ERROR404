1cc65de4c2f8aff8f025f795b631a10d9b632f2a
Lindner <A127E81@rewe-group.com>
2018-12-04 12:31:28 +0100

    burnrow + schriftart

--- javascript/credits.js
+++ javascript/credits.js
@@ -23,42 +23,43 @@ function init () {
     }
 
 }
+
 function drawBackbutton () {
     ctx.drawImage(button,backxpos,backypos,backxwidth,backyheight);
 
     ctx.fillStyle = "#000000";
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("Back", canvasWidth/2 - 25 , 675);
 
 
     ctx.fillStyle = "#FFFFFF";
-    ctx.font = "bold 24px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("ERROR404 - Arkanoid", 25, 25);
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("v1.0", 25, 50);
-    ctx.font = "bold 22px Arial";
+    ctx.font = "bold 28px Agency FB";
     ctx.fillText("Projektrahmen", 25, 100);
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("Dieses Projekt wurde im Rahmen der Vorlesung Projekte der Wirtschaftinformatik an der FHDW ", 25, 125);
     ctx.fillText("Bergisch Gladbach durchgeführt.", 25, 150);
-    ctx.font = "bold 22px Arial";
+    ctx.font = "bold 28px Agency FB";
     ctx.fillText("Autoren", 25, 200);
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("Julius Körner und Marcel Lindner", 25, 225);
-    ctx.font = "bold 22px Arial";
+    ctx.font = "bold 28px Agency FB";
     ctx.fillText("Lizenzen", 25, 275);
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("Eigen: MIT", 25, 300);
     ctx.fillText("Alle verwendeten Grafiken nutzen die Creative Commons Zero Lizens", 25, 325);
-    ctx.font = "bold 22px Arial";
+    ctx.font = "bold 28px Agency FB";
     ctx.fillText("How 2 play", 25, 375);
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("1. Im Menü auf Spiel starten klicken", 25, 400);
     ctx.fillText("2. A drücken", 25, 425);
     ctx.fillText("3. Mit der Maus das Pannel auf der unteren Seite bewegen und den Ball treffen", 25, 450);
-    ctx.font = "bold 22px Arial";
+    ctx.font = "bold 28px Agency FB";
     ctx.fillText("Known Bugs", 25, 500);
-    ctx.font = "bold 16px Arial";
+    ctx.font = "bold 24px Agency FB";
     ctx.fillText("  ", 25, 525);
     
     }
--- javascript/menu.js
+++ javascript/menu.js
@@ -10,6 +10,7 @@ var credxwidth = 200;
 var credypos = 350;
 var credyheight = 100;
 var background = new Image();
+var button = new Image();
 
 function init () {
     canvas = document.getElementById("canvas");
@@ -17,31 +18,39 @@ function init () {
     canvas.style.border = "pink 5px solid";
 
     background.src = "./textures/pinkishsky.jpg";
+    button.src = "./textures/buttontrans.png";
 
     background.onload = function(){
         ctx.drawImage(background,0,0,canvasWidth,canvasHeight);
+        drawHead();
         drawStartbutton();
         drawCredbutton();
     }
 }
-
+function drawHead () {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.font = "bold 48px Agency FB";
+    ctx.fillText("ERROR404", (canvas.width / 2) - 85, 120);
+}
 function drawStartbutton () {
-    ctx.fillStyle = "pink";
-    ctx.fillRect(startxpos,startypos,startxwidth,startyheight);
-    ctx.stroke();
+    ctx.drawImage(button,startxpos,startypos,startxwidth,startyheight);
+    //ctx.fillStyle = "pink";
+    //ctx.fillRect(startxpos,startypos,startxwidth,startyheight);
+    //ctx.stroke();
 
     ctx.fillStyle = "black";
-    ctx.font = "bold 16px Arial";
-    ctx.fillText("Start Game", (canvas.width / 2) - 45, (canvas.height / 2) -92);
+    ctx.font = "bold 24px Agency FB";
+    ctx.fillText("Start Game", (canvas.width / 2) - 45, (canvas.height / 2) -100);
     }
 function drawCredbutton () {
-    ctx.fillStyle = "pink";
-    ctx.fillRect(credxpos,credypos,credxwidth,credyheight);
-    ctx.stroke();
+    // ctx.fillStyle = "pink";
+    // ctx.fillRect(credxpos,credypos,credxwidth,credyheight);
+    // ctx.stroke();
+    ctx.drawImage(button,credxpos,credypos,credxwidth,credyheight);
     
     ctx.fillStyle = "black";
-    ctx.font = "bold 16px Arial";
-    ctx.fillText("Credits", (canvas.width / 2) - 30, (canvas.height / 2) +32);
+    ctx.font = "bold 24px Agency FB";
+    ctx.fillText("Credits", (canvas.width / 2) - 30, (canvas.height / 2) +25);
     }
 
 function startGame () {
